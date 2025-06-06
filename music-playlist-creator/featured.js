9b312589e954d1d0d7b34aa5b6c4f31aabd79a41
Nancy Cristina Farias <nancyfar@meta.com>
2025-06-06 16:02:02 -0700

    document featured.js

--- music-playlist-creator/featured.js
+++ music-playlist-creator/featured.js
@@ -2,16 +2,19 @@ const featuredPlaylistImage = document.getElementById("featuredPlaylistImage");
 const featuredPlaylistName = document.getElementById("featuredPlaylistName");
 const featuredPlaylistSongs = document.getElementById("featuredPlaylistSongs");
 
+// Fetch data from json
 fetch("data/data.json")
   .then(response => response.json())
   .then(playlists => {
     renderFeaturedPlaylist(loadRandomPlaylist(playlists));
   })
 
+// Load the randomly chosen playlist
 function loadRandomPlaylist(playlists) {
   return playlists[Math.floor(Math.random() * playlists.length)];
 }
 
+// Display the chosen playlist
 function renderFeaturedPlaylist(playlist) {
   featuredPlaylistImage.src = playlist.playlist_art;
   featuredPlaylistImage.alt = playlist.playlist_name;
@@ -19,9 +22,9 @@ function renderFeaturedPlaylist(playlist) {
   renderFeaturedSongs(playlist.songs);
 }
 
+// Display songs under chosen playlist
 function renderFeaturedSongs(songs) {
   featuredPlaylistSongs.innerHTML = "";
-
   songs.forEach(song => {
     const songCard = document.createElement("li");
     songCard.className = "featured-song-card";
